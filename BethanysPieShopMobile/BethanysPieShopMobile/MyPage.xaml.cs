e0e349e89fac8e1455dd12e66fb4bdfc5ec1c80d
venkat raman <warrol@venkats-MacBook-Pro.local>
2019-12-15 22:36:02 +0530

    first commit

---
+++ BethanysPieShopMobile/BethanysPieShopMobile/MyPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BethanysPieShopMobile
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BethanysPieShopMobile/BethanysPieShopMobile/NavigationPageView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace BethanysPieShopMobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavigationPageView : ContentPage
+    {
+        public NavigationPageView()
+        {
+            InitializeComponent();
+        }
+
+        private async void HomePageButton_Click(object sender, EventArgs e)
+        {
+            await this.Navigation.PushAsync(new MyPage());
+        }
+    }
+}
